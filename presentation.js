c74371582f95e65340406e64957b9dd916f35a0a
Guillaume Pierre <guillaumepierre56@gmail.com>
2019-09-18 12:38:54 +0200

    Ajout visualisation collegues

--- presentation.js
+++ presentation.js
@@ -1,40 +1,51 @@
-var login = require('./service.js');
-
-exports.start = start;
+var service = require('./service.js');
 var readline = require('readline');
+exports.start = start;
 
+// point d'entrée
 function start() {
-    login.login(function () {
-        afficherMenu();
-    }, function (error) {
-        console.log(error);
+
+    // Appel du scanner
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
     });
 
-    function authentification(login, motDePasse) {
+    console.log('Merci de vous identifier :');
+    rl.question('Nom d\'utilisateur : \n', function (utilisateur) {
+        rl.question('Mot de passe : \n', function (motDePasse) {
+            service.login(utilisateur, motDePasse, function () {
+                afficherMenu(rl);
+            })
+        })
+    });
+}
 
-    }
+function afficherMenu(rl) {
 
-    function afficherMenu() {
-        console.log('1. Rechercher un collègue par nom');
-        console.log('99. Sortir');
+    console.log('1. Rechercher un collègue par nom');
+    console.log('99. Sortir');
 
 
-        var r1 = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
+    rl.question('Choisissez une action : \n', function (saisie) {
+        if (saisie == 1) {
+            rl.question('Nom de la personne à rechercher :', function (nomCollegue) {
+                console.log('>> Recherche en cours du nom ' + nomCollegue);
+                service.recherche(nomCollegue, function(callbackFn) {
+                    service.recuperer(callbackFn, function (resultat) {
+                        console.log(resultat);
+                    });
+                } )
 
-        r1.question('Choisissez une action : \n', function (saisie) {
-            if (saisie == 1) {
-                console.log('>> Recherche en cours du nom XXX');
-            } else if (saisie == 99) {
-                console.log('Au revoir');
-            }
+            })
+        } else if (saisie == 99) {
+            console.log('Au revoir');
+            rl.close();
+        }
 
-            r1.close();
-        });
 
-    }
+    });
+
 
 }
 
--- service.js
+++ service.js
@@ -1,29 +1,84 @@
-var request = require('request').defaults({jar:true});
+var request = require('request').defaults({jar: true});
 exports.login = login;
+exports.recherche = recupererMatricule;
+exports.recuperer = recupererInfos;
 
 
-    function login(login, motDePasse, callbackFn, error) {
 
-        request('http://localhost:8080/auth', {
-                method: 'POST',
-                json: true,
-                body: {
-                    nomUtilisateur: "jojo",
-                    motDePasse: "1234"
-                }
-            },
-            function resp(err, res, body) {
+function login(login, motDePasse, callbackFn) {
 
-                console.log(res.statusCode);
-                if (res.statusCode === 200) {
-                    callbackFn(res);
-                } else {
-                    error("Mauvais identifiants");
-                }
+    request('https://guillaume-top-collegues.herokuapp.com/auth', {
+            method: 'POST',
+            json: true,
+            body: {
+                nomUtilisateur: login,
+                motDePasse: motDePasse
             }
+        },
+        function resp(err, res, body) {
 
-            );
+            console.log(res.statusCode);
+            if (res.statusCode === 200) {
+                callbackFn();
+            } else {
+                console.log('Erreur d\'authentification');
+            }
+        }
+    );
 
 
-    }
+}
+
+function recupererMatricule(nom, callbackFn, errorFn) {
+    console.log('Nom récupéré :' + nom);
+    request('https://guillaume-top-collegues.herokuapp.com/collegue?nomCollegue=' + nom, {
+            method: 'GET',
+            json: true
+        },
+        function resp(err, res, body) {
+            console.log('Exécution de la récupération des collegues avec le nom sélectionné...');
+            console.log('Réponse du serveur :' + res.statusCode);
+            if (res.statusCode === 200) {
+                console.log('Corps de la réponse :' + body);
+                callbackFn(body);
+
+
+            } else {
+                errorFn('Aucune information pour ce nom');
+            }
+        }
+    )
+}
+
+
+function recupererInfos(body, callbackFn2) {
+    console.log('Matricule inséré dans la requête :' + body);
+
+
+        var nbMats = body.length;
+
+        retour = [];
+        body.forEach(function(matricule){
+
+            request('https://guillaume-top-collegues.herokuapp.com/collegue/' + matricule, {
+                    method: 'GET',
+                    json: true
+                },
+                function resp(err, res, body) {
+                    retour.push(body);
+
+                    if(nbMats === retour.length) {
+                        callbackFn2(retour);
+                    }
+
+                });
+        });
+
+
+
+
+
+}
+
+
 
