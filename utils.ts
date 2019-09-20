3656f0dbaace8c2908be64e26d3f18ab0a817003
Guillaume Pierre <guillaumepierre56@gmail.com>
2019-09-20 17:10:47 +0200

    Début refactoring

---
+++ utils.ts
@@ -0,0 +1,17 @@
+import moment from "moment";
+
+export class Utils {
+
+    convertirDate(dateAConvertir:string):Date {
+        let jour:number = parseInt(dateAConvertir.substr(0, 1));
+        let mois:number = parseInt(dateAConvertir.substr(3,4));
+        let annee:number = parseInt(dateAConvertir.substr(6,9));
+        return new Date(annee, mois, jour);
+    }
+
+
+
+    dateToLocalDate(dateAConvertir:Date):string {
+       return `${dateAConvertir.getFullYear()}-${dateAConvertir.getMonth()}-${dateAConvertir.getDay()}`;
+    }
+}
